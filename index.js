34b31b0f0a545767206564e745b70bbce49d54f2
Jonathan Viera <156127051+jviera100@users.noreply.github.com>
2024-03-29 09:58:06 -0300

    :sparkles: cambio de nombre imagen al ser creada con ruta de prueba escrita en input de pagina web, se genera imagen con uuid

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const chalk = require('chalk');
 const { v4: uuidv4 } = require('uuid');
 const Jimp = require('jimp');
 const path = require('path');
-//http://placedog.net/200/200
+
+//PAGINAS DE PRUEBA
+//http://placedog.net/200/200  https://placekitten.com/200/200  https://placebear.com/200/200
 
 const app = express();
 const PORT = 3000;
@@ -12,7 +14,8 @@ const l = console.log; //variable que almacena console.log
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('assets'));
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    res.setHeader('Content-Type', 'image/png')
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
@@ -20,9 +23,13 @@ app.post('/procesar-imagen', async (req, res) => {
     const { imagenUrl } = req.body;
     try {
         const image = await Jimp.read(imagenUrl);
-        const processedImage = await image.resize(350, Jimp.AUTO).grayscale().quality(80);
-        const nombreImagen = uuidv4() + '.jpg';
-        await processedImage.writeAsync(path.join(__dirname, '/assets/img/' + nombreImagen)); // Almacena la imagen procesada
+        const processedImage = await image
+            .resize(350, Jimp.AUTO) //ancho y alto
+            .sepia()  //sepia negra
+            .grayscale() //grises 
+            .quality(80); //calidad
+        const nombreImagen = uuidv4().slice(0, 4) + '_uuid_foto.jpg'; // Genera un nombre de imagen aleatorio
+        await processedImage.writeAsync(path.join(__dirname, '/assets/img/' + nombreImagen)); // Almacena la imagen procesada con nombre
         res.sendFile(path.join(__dirname, '/assets/img/' + nombreImagen)); // Muestra la imagen procesada
     } catch (error) {
         res.status(500).send('Error al procesar la imagen.');
